b9b255becdbdc678a521793dfcc2b327027b1630
kashukov <andrey@kashukov.com>
2014-08-16 22:44:27 +0300

    Initial commit.

---
+++ src/main/java/com/kashukov/Convert.java
@@ -0,0 +1,14 @@
+package com.kashukov;
+
+public class Convert {
+
+    /**
+     * Convert short to String
+     *
+     * @param input short
+     * @return String
+     */
+    public static String shortToString(short input) {
+        return Short.toString(input);
+    }
+}
---
+++ src/test/java/com/kashukov/ConvertTest.java
@@ -0,0 +1,12 @@
+package com.kashukov;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConvertTest {
+
+    @Test
+    public void testShortToString() throws Exception {
+        Assert.assertEquals(1, 1);
+    }
+}
